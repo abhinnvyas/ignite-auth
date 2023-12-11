772d1ad8a7d7a3fe3d44ae1f13ca54177bf41aa9
abhinn2@github <abhinnvyas2@gmail.com>
2023-12-11 14:39:30 +0530

    Decreased the size of all elements on the page

--- app/user/page.js
+++ app/user/page.js
@@ -17,12 +17,12 @@ function page() {
       </div>
       <main>
         <div className={`${textPrimary} p-4  mt-5 `}>
-          <h1 className="text-xl md:text-3xl lg:text-4xl font-semibold">
+          <h1 className="text-md md:text-2xl lg:text-4xl font-semibold">
             Welcome,
             <span className={`${textSecondary} font-bold`}> User</span>
           </h1>
           <h1
-            className={`text-md md:text-lg lg:text-xl font-normal mt-2  ${textExtraLight}`}
+            className={`text-md md:text-md lg:text-md font-normal mt-2  ${textExtraLight}`}
           >
             Accept payments online hassle-free with our QR code service.
           </h1>
@@ -30,39 +30,39 @@ function page() {
 
         <div class="flex flex-col border-2 rounded-lg  justify-left p-2 bg-white">
           <div className="p-2">
-            <h1 className="text-xl font-semibold ">Recent Activity</h1>
+            <h1 className="text-md font-semibold ">Recent Activity</h1>
           </div>
           <div class="p-6 overflow-auto px-0">
             <table class="w-full table-auto text-left">
               <thead className="">
                 <tr>
                   <th class="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4">
-                    <p class="block antialiased font-sans text-lg text- font-normal leading-none opacity-70">
+                    <p class="block antialiased font-sans text-md text- font-normal leading-none opacity-70">
                       #
                     </p>
                   </th>
                   <th class="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4">
-                    <p class="block antialiased font-sans text-lg text-blue-gray-900 font-normal leading-none opacity-70">
+                    <p class="block antialiased font-sans text-md text-blue-gray-900 font-normal leading-none opacity-70">
                       Customer Name
                     </p>
                   </th>
                   <th class="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4">
-                    <p class="block antialiased font-sans text-lg text-blue-gray-900 font-normal leading-none opacity-70">
+                    <p class="block antialiased font-sans text-md text-blue-gray-900 font-normal leading-none opacity-70">
                       Client Txn ID
                     </p>
                   </th>
                   <th class="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4">
-                    <p class="block antialiased font-sans text-lg text-blue-gray-900 font-normal leading-none opacity-70">
+                    <p class="block antialiased font-sans text-md text-blue-gray-900 font-normal leading-none opacity-70">
                       Amount
                     </p>
                   </th>
                   <th class="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4">
-                    <p class="block antialiased font-sans text-lg text-blue-gray-900 font-normal leading-none opacity-70">
+                    <p class="block antialiased font-sans text-md text-blue-gray-900 font-normal leading-none opacity-70">
                       Status
                     </p>
                   </th>
                   <th class="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4">
-                    <p class="block antialiased font-sans text-lg text-blue-gray-900 font-normal leading-none opacity-70">
+                    <p class="block antialiased font-sans text-md text-blue-gray-900 font-normal leading-none opacity-70">
                       Date
                     </p>
                   </th>
@@ -73,7 +73,7 @@ function page() {
                   <td class="p-4 border-b border-blue-gray-50">
                     <div class="flex items-center gap-3">
                       <p
-                        class={`block antialiased font-sans text-lg leading-normal ${textSecondary} `}
+                        class={`block antialiased font-sans text-md leading-normal ${textSecondary} `}
                       >
                         1
                       </p>
@@ -81,35 +81,35 @@ function page() {
                   </td>
                   <td class="p-4 border-b border-blue-gray-50">
                     <div class="flex items-center gap-3">
-                      <p class="block antialiased font-sans text-lg leading-normal text-blue-gray-900 ">
+                      <p class="block antialiased font-sans text-md leading-normal text-blue-gray-900 ">
                         Test Name
                       </p>
                     </div>
                   </td>
                   <td class="p-4 border-b border-blue-gray-50">
                     <div class="flex items-center gap-3">
-                      <p class="block antialiased font-sans text-lg leading-normal text-blue-gray-900 ">
+                      <p class="block antialiased font-sans text-md leading-normal text-blue-gray-900 ">
                         000111
                       </p>
                     </div>
                   </td>
                   <td class="p-4 border-b border-blue-gray-50">
                     <div class="flex items-center gap-3">
-                      <p class="block antialiased font-sans text-lg leading-normal text-blue-gray-900 ">
+                      <p class="block antialiased font-sans text-md leading-normal text-blue-gray-900 ">
                         500$
                       </p>
                     </div>
                   </td>
                   <td class="p-4 border-b border-blue-gray-50">
                     <div class="flex items-center gap-3">
-                      <p class="block antialiased font-sans text-lg leading-normal text-blue-gray-900  bg-yellow-400 text-white p-2 rounded-lg">
+                      <p class="block antialiased font-sans text-md leading-normal text-blue-gray-900  bg-yellow-400 text-white p-2 rounded-lg">
                         Created
                       </p>
                     </div>
                   </td>
                   <td class="p-4 border-b border-blue-gray-50">
                     <div class="flex items-center gap-3">
-                      <p class="block antialiased font-sans text-lg leading-normal text-blue-gray-900  ">
+                      <p class="block antialiased font-sans text-md leading-normal text-blue-gray-900  ">
                         29-11-2023 12:29 PM
                       </p>
                     </div>
