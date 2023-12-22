74b571ec136e6b1ca14d99616e846708724e0e00
abhinn2@github <abhinnvyas2@gmail.com>
2023-12-22 13:38:54 +0530

    Completed API keys and Webhooks.

--- components/API Credentials/Section2.jsx
+++ components/API Credentials/Section2.jsx
@@ -5,16 +5,16 @@ import { KeyIcon, ClipboardIcon } from "@heroicons/react/24/outline";
 function Section2() {
   const [ApiKey, setApiKey] = useState("random-api-key");
   return (
-    <div className="flex w-full items-center space-x-4">
-      <div className="w-40 p-4">
+    <div className="flex w-full items-center space-x-4 border-2 p-2 rounded-lg">
+      <div className="w-40 p-4 border-r-2">
         <KeyIcon className=" bg-my_secondary text-my_background rounded-lg p-4" />
       </div>
-      <div className="flex-1">
+      <div className="flex-1 w-full">
         <h1 className="font-semibold text-2xl mb-5">API Key</h1>
         <h1 className="font-normal text-xl mb-2">Key</h1>
-        <div className="flex space-x-4 items-center ">
+        <div className="flex space-x-4 items-center  ">
           <input
-            className="bg-my_extra_light w-fit h-12 p-2 text-lg text-my_light rounded-lg opacity-80 border-2 border-gray-400 px-4"
+            className="bg-my_extra_light w-fit h-12 p-2 text-lg text-my_light rounded-lg opacity-80 border-2 border-gray-400 px-4 flex-1 max-w-lg"
             type="text"
             placeholder
             value={ApiKey}
