f3eb123bc6c6847fdf02b652a2becaf0d6784566
abhinn2@github <abhinnvyas2@gmail.com>
2023-12-26 15:15:26 +0530

    created the api_Credentials feature

--- app/api/api_credentials/getApiKey/index.js
+++ app/api/api_credentials/getApiKey/index.js
@@ -17,3 +17,20 @@ export async function getApiKey(clientid, token) {
     throw error;
   }
 }
+
+export async function regenerateApiKey(clientid, token) {
+  try {
+    const headers = {
+      clientid: clientid,
+      token: token,
+    };
+
+    const res = await axios.post(`${BASE_URL}/APIKey/regenerateAPIKey`, null, {
+      headers,
+    });
+
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+}
--- components/API Credentials/Section2.jsx
+++ components/API Credentials/Section2.jsx
@@ -1,18 +1,39 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { KeyIcon, ClipboardIcon } from "@heroicons/react/24/outline";
-import { getApiKey } from "@/app/api/api_credentials/getApiKey";
+import {
+  getApiKey,
+  regenerateApiKey,
+} from "@/app/api/api_credentials/getApiKey";
 import Cookies from "js-cookie";
 
 function Section2() {
-  const [ApiKey, setApiKey] = useState("random-api-key");
+  const [ApiKey, setApiKey] = useState("your-api-key");
   useEffect(() => {
     const clientId = Cookies.get("ClientId");
     const token = Cookies.get("token");
     getApiKey(clientId, token).then((res) => {
-      console.log(res);
+      setApiKey(res.APIKey);
     });
   }, []);
+
+  const regeneratekey = async (e) => {
+    e.preventDefault();
+    const clientId = Cookies.get("ClientId");
+    const token = Cookies.get("token");
+    await regenerateApiKey(clientId, token).then((res) => {
+      setApiKey(res.APIKey);
+    });
+  };
+
+  const handleCopyClick = async (e) => {
+    e.preventDefault();
+    try {
+      await navigator.clipboard.writeText(ApiKey);
+      alert("Text copied to clipboard!");
+    } catch (error) {}
+  };
+
   return (
     <div className="flex w-full items-center space-x-4 border-2 p-2 rounded-lg">
       <div className="w-40 p-4 border-r-2">
@@ -23,16 +44,22 @@ function Section2() {
         <h1 className="font-normal text-xl mb-2">Key</h1>
         <div className="flex space-x-4 items-center  ">
           <input
-            className="bg-my_extra_light w-fit h-12 p-2 text-lg text-my_light rounded-lg opacity-80 border-2 border-gray-400 px-4 flex-1 max-w-lg"
+            className="bg-my_extra_light w-fit h-12 p-2 text-lg text-my_light rounded-lg opacity-80 border-2 border-gray-400 px-4 flex-1 max-w-lg cursor-not-allowed unselectable"
             type="text"
             placeholder
             value={ApiKey}
             disabled
           />
-          <button className=" bg-my_secondary text-white text-lg px-4 rounded-lg hover:shadow-lg transition-all ease-in-out p-2">
+          <button
+            onClick={regeneratekey}
+            className=" bg-my_secondary text-white text-lg px-4 rounded-lg hover:shadow-lg transition-all ease-in-out p-2"
+          >
             Change Credentials
           </button>
-          <button className="bg-white border-2 border-my_secondary hover:bg-my_secondary text-lg p-2 rounded-lg hover:shadow-lg transition-all ease-in-out flex items-center justify-center ">
+          <button
+            onClick={handleCopyClick}
+            className="bg-white border-2 border-my_secondary hover:bg-my_secondary text-lg p-2 rounded-lg hover:shadow-lg transition-all ease-in-out flex items-center justify-center "
+          >
             <ClipboardIcon className="w-8 h-8 text-my_secondary hover:text-white" />
           </button>
         </div>
