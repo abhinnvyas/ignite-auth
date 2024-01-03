23ee3d0c3afc1e31fef89e6b6917276a4592ef59
abhinn2@github <abhinnvyas2@gmail.com>
2024-01-03 12:46:31 +0530

    Redesigned the buy-Credits page

--- components/Buy Credits/Section2.jsx
+++ components/Buy Credits/Section2.jsx
@@ -7,9 +7,18 @@ import Lottie from "lottie-react";
 function Section2() {
   return (
     <div className="p-4 rounded-lg mb-4">
-      <div className="grid grid-cols-3  ">
-        <div className="col-span-2  flex items-center justify-center p-4">
-          <Card />
+      <div className="grid lg:grid-cols-3 grid-rows-2  ">
+        <div className="col-span-2  flex  items-center justify-center p-4">
+          <div className="flex flex-col space-y-4">
+            <Card />
+            <div className="flex flex-col space-y-2">
+              <p>
+                Enjoy cost-effective communication with our credit system. For
+                just 0.1 rupees per credit, you can send one message, making it
+                an economical option for staying in in business.
+              </p>
+            </div>
+          </div>
         </div>
         <div className=" col-span-1">
           <Lottie animationData={animationData} height={100} width={100} />
